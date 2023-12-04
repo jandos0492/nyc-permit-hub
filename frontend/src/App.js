193f408f6d1640658a60e2d619982e13b5c62329
jandos0492 <jandos0492@gmail.com>
2023-12-04 01:36:08 -0500

    Added the CDLAirBrakesFullTestPage component

--- frontend/src/App.js
+++ frontend/src/App.js
@@ -1,52 +1,3 @@
-// import React, { useState, useEffect } from 'react';
-// import { useDispatch, useSelector } from 'react-redux';
-// import { Route, Routes } from 'react-router-dom';
-// import LoginFormPage from './components/LoginFormPage';
-// import SignupFormPage from './components/SignupFormPage';
-// import * as sessionActions from './store/session';
-// import Navigation from './components/Navigation';
-// import PasswordResetRequest from './components/PasswordResetRequest';
-// import ResetPassword from './components/ResetPassword';
-// import Home from './components/Home';
-// import AutoEnglishHomePage from './components/AutoEnglishHomePage';
-// import AutoLearnPage from './components/AutoLearnPage';
-// import TrafficSignsPage from './components/TrafficSignsPage';
-// import AutoFullTestPage from './components/AutoFullTestPage';
-// import AutoExpressTestPage from './components/AutoExpressTestPage';
-
-// function App() {
-//   const dispatch = useDispatch();
-//   const [isLoaded, setIsLoaded] = useState(false);
-//   const isAuthenticated = useSelector((state) => Boolean(state.session.user));
-
-//   useEffect(() => {
-//     dispatch(sessionActions.restoreUser()).then(() => setIsLoaded(true));
-//   }, [dispatch]);
-
-//   return (
-//     <>
-//       <Navigation isLoaded={isLoaded} />
-//       {isLoaded && (
-//         <Routes>
-//           {!isAuthenticated && <Route path="/login" element={<LoginFormPage />} />}
-//           {!isAuthenticated && <Route path="/signup" element={<SignupFormPage />} />}
-//           {!isAuthenticated && <Route path="/reset-password" element={<PasswordResetRequest />} />}
-//           {!isAuthenticated && <Route path="/reset-password/:token" element={<ResetPassword />} />}
-
-//           {isAuthenticated && <Route path="/" element={<Home />} />}
-//           {isAuthenticated && <Route path="/auto" element={<AutoEnglishHomePage />} />}
-//           {isAuthenticated && <Route path="/auto/english/learn" element={<AutoLearnPage />} />}
-//           {isAuthenticated && <Route path="/auto/english/traffic-signs/learn" element={<TrafficSignsPage />} />}
-//           {isAuthenticated && <Route path="/auto/english/full-test" element={<AutoFullTestPage />} />}
-//           {isAuthenticated && <Route path="/auto/english/express-test" element={<AutoExpressTestPage />} />}
-//         </Routes>
-//       )}
-//     </>
-//   );
-// }
-
-// export default App;
-
 import React, { useState, useEffect } from 'react';
 import { useDispatch, useSelector } from 'react-redux';
 import { Route, Routes } from 'react-router-dom';
@@ -75,6 +26,7 @@ import CDLGeneralKnowledgeFullTestPage from './components/CDL/CDLGeneralKnowledg
 import CDLGeneralKnowledgeExpressTestPage from './components/CDL/CDLGeneralKnowledgeExpressTestPage';
 import CDLAirBrakesHomePage from "./components/CDL/CDLAirBrakesHomePage";
 import CDLAirBrakesLearnPage from './components/CDL/CDLAirBrakesLearnPage';
+import CDLAirBrakesFullTestPage from './components/CDL/CDLAirBrakesFullTestPage';
 
 function App() {
   const dispatch = useDispatch();
@@ -114,6 +66,7 @@ function App() {
           {isAuthenticated && <Route path="/cdl/general-knowledge/express-test" element={<CDLGeneralKnowledgeExpressTestPage />} />}
           {isAuthenticated && <Route path="/cdl/air-brakes" element={<CDLAirBrakesHomePage />} />}
           {isAuthenticated && <Route path="/cdl/air-brakes/learn" element={<CDLAirBrakesLearnPage />} />}
+          {isAuthenticated && <Route path="/cdl/air-brakes/full-test" element={<CDLAirBrakesFullTestPage />} />}
         </Routes>
       )}
     </>
