9e822d796e7254ae31bc6c51a028707ef34967d4
jandos0492 <jandos0492@gmail.com>
2024-12-08 02:18:07 -0500

    Added the correct and wrong logic to the GeneralKnowledgeFullTest  component, user can now see the correct and wrong answers by couting them and also the percentage of the passing the test in live mode

--- frontend/src/pages/cdl/generalKnowledge/GeneralKnowledgeFullTest/index.jsx
+++ frontend/src/pages/cdl/generalKnowledge/GeneralKnowledgeFullTest/index.jsx
@@ -17,6 +17,8 @@ const GeneralKnowledgeFullTest = () => {
     const [selectedAnswers, setSelectedAnswers] = useState({});
     const [countCorrectAnswers, setCountCorrectAnswers] = useState(0);
     const [isModalOpen, setIsModalOpen] = useState(false);
+    const [correct, setCorrect] = useState(0);
+    const [wrong, setWrong] = useState(0);
     const userId = useSelector((state) => state?.session?.user?.id);
 
     useEffect(() => {
@@ -31,12 +33,16 @@ const GeneralKnowledgeFullTest = () => {
     const handleSubmit = () => {
         testResult.sendTestResult({
             userId,
-            score: String(calculatePercentage()),
+            score: String(
+                ((correct / generalKnowledgeFullTestData.length) * 100).toFixed(
+                    0
+                )
+            ),
             vehicleType: "cdl general knowledge",
             testType: "full test",
             testLanguage:
                 "https://algify-videos.s3.us-east-2.amazonaws.com/nyc-permit-hub-images/english-flag.jpg",
-            pass: calculatePercentage() >= 70,
+            pass: ((correct / generalKnowledgeFullTestData.length) * 100).toFixed(0) >= 70,
             requiredScore: "70",
         });
 
@@ -76,12 +82,17 @@ const GeneralKnowledgeFullTest = () => {
             ...prev,
             [questionId]: answerIdx,
         }));
-    };
 
-    const calculatePercentage = () => {
-        const totalQuestions = generalKnowledgeFullTestData.length;
-        const percentage = (countCorrectAnswers / totalQuestions) * 100;
-        return percentage.toFixed(0);
+        const isCorrect =
+            generalKnowledgeFullTestData[generalKnowledgeFullTestIdx].correctAnswerIndex ===
+            answerIdx;
+
+        if (isCorrect) {
+            setCorrect((prev) => prev + 1);
+            setCountCorrectAnswers((prevCount) => prevCount + 1);
+        } else {
+            setWrong((prev) => prev + 1);
+        }
     };
 
     return (
@@ -95,6 +106,27 @@ const GeneralKnowledgeFullTest = () => {
                         onSubmit={(e) => e.preventDefault()}
                         className=" flex flex-col items-center bg-cyan-50 h-screen pt-20 md:pt-48"
                     >
+                        <div className="w-full flex justify-center bg-cyan-50 pb-12">
+                            <div className="flex justify-around w-full max-w-xs md:max-w-lg">
+                                <div className="flex items-center justify-center bg-green-100 text-green-700 border border-green-300 font-lato text-xs md:text-lg p-2 md:p-4 rounded-lg shadow-md">
+                                    Correct: {correct}
+                                </div>
+                                {correct > 0 && (
+                                    <div className="flex items-center justify-center bg-cyan-800 border border-cyan-300 font-bold font-lato text-white text-xs md:text-lg p-2 md:p-4  rounded-lg shadow-md">
+                                        Score:{" "}
+                                        {(
+                                            (correct /
+                                                generalKnowledgeFullTestData.length) *
+                                            100
+                                        ).toFixed(0)}
+                                        %
+                                    </div>
+                                )}
+                                <div className="flex items-center justify-center bg-red-100 text-red-700 border border-red-300 font-lato text-xs md:text-lg p-2 md:p-4 rounded-lg shadow-md">
+                                    Wrong: {wrong}
+                                </div>
+                            </div>
+                        </div>
                         <div className="flex justify-center items-center">
                             <NavButton
                                 icon="fa-circle-chevron-left"
@@ -170,7 +202,10 @@ const GeneralKnowledgeFullTest = () => {
                         setIsModalOpen(false);
                         window.location.reload();
                     }}
-                    score={calculatePercentage()}
+                    score={(
+                        (correct / generalKnowledgeFullTestData.length) *
+                        100
+                    ).toFixed(0)}
                 />
             )}
         </>
