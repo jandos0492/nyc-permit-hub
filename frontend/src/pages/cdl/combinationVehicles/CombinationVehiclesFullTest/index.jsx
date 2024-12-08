276925cfe13c0d91ceb209f9a976d6496d82b849
jandos0492 <jandos0492@gmail.com>
2024-12-08 03:00:11 -0500

    Added the correct and wrong logic to the CombinationVehiclesFullTest  component, user can now see the correct and wrong answers by couting them and also the percentage of the passing the test in live mode

--- frontend/src/pages/cdl/combinationVehicles/CombinationVehiclesFullTest/index.jsx
+++ frontend/src/pages/cdl/combinationVehicles/CombinationVehiclesFullTest/index.jsx
@@ -16,6 +16,8 @@ const CombinationVehiclesFullTest = () => {
         useState(0);
     const [selectedAnswers, setSelectedAnswers] = useState({});
     const [countCorrectAnswers, setCountCorrectAnswers] = useState(0);
+    const [correct, setCorrect] = useState(0);
+    const [wrong, setWrong] = useState(0);
     const [isModalOpen, setIsModalOpen] = useState(false);
     const userId = useSelector((state) => state?.session?.user?.id);
 
@@ -31,12 +33,20 @@ const CombinationVehiclesFullTest = () => {
     const handleSubmit = () => {
         testResult.sendTestResult({
             userId,
-            score: String(calculatePercentage()),
+            score: String(
+                (
+                    (correct / combinationVehiclesFullTestData.length) *
+                    100
+                ).toFixed(0)
+            ),
             vehicleType: "cdl combination vehicles",
             testType: "full test",
             testLanguage:
                 "https://algify-videos.s3.us-east-2.amazonaws.com/nyc-permit-hub-images/english-flag.jpg",
-            pass: calculatePercentage() >= 70,
+            pass:
+                ((correct / combinationVehiclesFullTestData.length) * 100).toFixed(
+                    0
+                ) >= 70,
             requiredScore: "70",
         });
 
@@ -76,12 +86,17 @@ const CombinationVehiclesFullTest = () => {
             ...prev,
             [questionId]: answerIdx,
         }));
-    };
 
-    const calculatePercentage = () => {
-        const totalQuestions = combinationVehiclesFullTestData.length;
-        const percentage = (countCorrectAnswers / totalQuestions) * 100;
-        return percentage.toFixed(0);
+        const isCorrect =
+            combinationVehiclesFullTestData[combinationVehiclesFullTestIdx].correctAnswerIndex ===
+            answerIdx;
+
+        if (isCorrect) {
+            setCorrect((prev) => prev + 1);
+            setCountCorrectAnswers((prevCount) => prevCount + 1);
+        } else {
+            setWrong((prev) => prev + 1);
+        }
     };
 
     return (
@@ -95,6 +110,27 @@ const CombinationVehiclesFullTest = () => {
                         onSubmit={(e) => e.preventDefault()}
                         className=" flex flex-col items-center bg-cyan-50 h-screen pt-20 md:pt-48"
                     >
+                        <div className="w-full flex justify-center bg-cyan-50 pb-12">
+                            <div className="flex justify-around w-full max-w-xs md:max-w-lg">
+                                <div className="flex items-center justify-center bg-green-100 text-green-700 border border-green-300 font-lato text-xs md:text-lg p-2 md:p-4 rounded-lg shadow-md">
+                                    Correct: {correct}
+                                </div>
+                                {correct > 0 && (
+                                    <div className="flex items-center justify-center bg-cyan-800 border border-cyan-300 font-bold font-lato text-white text-xs md:text-lg p-2 md:p-4  rounded-lg shadow-md">
+                                        Score:{" "}
+                                        {(
+                                            (correct /
+                                                combinationVehiclesFullTestData.length) *
+                                            100
+                                        ).toFixed(0)}
+                                        %
+                                    </div>
+                                )}
+                                <div className="flex items-center justify-center bg-red-100 text-red-700 border border-red-300 font-lato text-xs md:text-lg p-2 md:p-4 rounded-lg shadow-md">
+                                    Wrong: {wrong}
+                                </div>
+                            </div>
+                        </div>
                         <div className="flex justify-center items-center">
                             <NavButton
                                 icon="fa-circle-chevron-left"
@@ -170,7 +206,10 @@ const CombinationVehiclesFullTest = () => {
                         setIsModalOpen(false);
                         window.location.reload();
                     }}
-                    score={calculatePercentage()}
+                    score={(
+                        (correct / combinationVehiclesFullTestData.length) *
+                        100
+                    ).toFixed(0)}
                 />
             )}
         </>
